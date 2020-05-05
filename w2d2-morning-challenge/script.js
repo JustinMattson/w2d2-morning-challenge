b9fd66d64d7a4db420424a7394abfb1a96830d09
Justin Mattson <justin.mattson@outlook.com>
2020-05-05 10:11:14 -0600

    initial commit

---
+++ w2d2-morning-challenge/script.js
@@ -0,0 +1,8 @@
+let myObject = {
+  type: "Mug",
+  material: "Stainless",
+  name: "Happiness",
+  ounces: 16,
+  contains: ["coffee", "water", "mixed drinks", "ale", "Scotch"],
+  empty: true,
+};
